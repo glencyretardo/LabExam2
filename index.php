97f0799a136c980bc49d6989e41662fc4244fc99
glencyretardo <g.retardo.534306@umindanao.edu.ph>
2024-02-22 16:47:39 +0800

    first commit

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ if (isset($_POST['submit'])) {
         }
     } else {
         
-        $errorMess = "Please type in your next travel !";
+        $errorMess = "Please type in your next travelll !";
     }
 }
 
